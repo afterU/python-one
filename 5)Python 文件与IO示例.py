f8bffd8f3dbb96ed18e450d8a2252d0000ae46eb
huangzw <zhiwei.huang@metawall.ai>
2023-03-14 18:14:04 +0800

    io

--- 5)Python 文件与IO示例.py
+++ 5)Python 文件与IO示例.py
@@ -156,11 +156,18 @@ print(m1)
 
     # Python 内存映射的二进制文件
     # 使用 mmap 模块来内存映射文件
-
+    # mmap() 返回的 mmap 对象同样也可以作为一个上下文管理器来使用， 这时候底层的文件会被自动关闭
+    # memeory_map() 函数打开的文件同时支持读和写操作。 任何的修改内容都会复制回原来的文件中。 如果需要只读的访问模式，可以给参数 access 赋值为 mmap.ACCESS_READ
+    # 如果你想在本地修改数据，但是又不想将修改写回到原始文件中，可以使用 mmap.ACCESS_COPY
+    # 为了随机访问文件的内容，使用 mmap 将文件映射到内存中是一个高效和优雅的方法。 例如，你无需打开一个文件并执行大量的 seek() ， read() ， write() 调用， 只需要简单的映射文件并使用切片操作访问数据即可。
+    # 一般来讲， mmap() 所暴露的内存看上去就是一个二进制数组对象。 但是，你可以使用一个内存视图来解析其中的数据
+    # 需要强调的一点是，内存映射一个文件并不会导致整个文件被读取到内存中。 也就是说，文件并没有被复制到内存缓存或数组中。相反，操作系统仅仅为文件内容保留了一段虚拟内存。 当你访问文件的不同区域时，这些区域的内容才根据需要被读取并映射到内存区域中。 而那些从没被访问到的部分还是留在磁盘上。所有这些过程是透明的，在幕后完成！
+    # 如果多个Python解释器内存映射同一个文件，得到的 mmap 对象能够被用来在解释器直接交换数据。 也就是说，所有解释器都能同时读写数据，并且其中一个解释器所做的修改会自动呈现在其他解释器中。 很明显，这里需要考虑同步的问题。但是这种方法有时候可以用来在管道或套接字间传递数据。
+    # 这一小节中函数尽量写得很通用，同时适用于Unix和Windows平台。 要注意的是使用 mmap() 函数时会在底层有一些平台的差异性。 另外，还有一些选项可以用来创建匿名的内存映射区域
 import mmap
 def memory_map(filename, access = mmap.ACCESS_WRITE):
     size = os.path.getsize(filename)
-    fd = open(filename, 'wb')
+    fd = os.open(filename, os.O_RDWR)
     return mmap.mmap(fd, size, access=access)
 size = 1000000
 with open(os.path.dirname(__file__) + '/mmap.log','wb') as f:
@@ -171,10 +178,54 @@ m = memory_map(os.path.dirname(__file__) + '/mmap.log')
 print(len(m))
 print(m[0:10])
 
+with memory_map(os.path.dirname(__file__) + '/mmap.log') as m:
+    print(len(m))
+    print(m[0:10])
+print(m.closed)
 
-
-
-
+    # Python 文件路径名的操作
+    # 可以用路径名来获取文件名，目录名，绝对路径
+path = os.path.dirname(__file__) + '/dstool.sh'
+print(path)
+    # 获取文件名
+print(os.path.basename(path))
+    # 文件所在目录
+print(os.path.dirname(path))
+    # 补全用户的目录
+user_path = '~/me/pythonworkspace/python-one/dstool.sh'
+print(os.path.expanduser(user_path))
+    # Python 测试文件是否存在
+os.path.exists('/etc/passwd')
+    # 是否是文件
+os.path.isfile('/etc/passwd')
+    # 是否是文件夹
+os.path.isdir('/etc/passwd')
+    # 是否是连接
+print(os.path.islink('/usr/local/bin/python3'))
+    # 获取真实连接
+print(os.path.realpath('/usr/local/bin/python3'))
+    # 获取文件大小
+print(os.path.getsize('/etc/passwd'))
+    # 获取文件的修改日期
+print(os.path.getmtime('/etc/passwd'))
+    # Python 获取文件夹中的文件列表
+names = os.listdir(os.path.dirname(__file__))
+print(names)
+names = [name for name in os.listdir(os.path.dirname(__file__))
+        if os.path.isfile(os.path.join(os.path.dirname(__file__), name))]
+print(names)
+    # Python 增加或改变已打开文件的编码
+    # 给一个以二进制模式打开的文件添加Unicode编码/解码方式， 可以使用 io.TextIOWrapper() 对象包装它
+    # io.TextIOWrapper 是一个编码和解码Unicode的文本处理层， io.BufferedWriter 是一个处理二进制数据的带缓冲的I/O层， io.FileIO 是一个表示操作系统底层文件描述符的原始文件。 增加或改变文本编码会涉及增加或改变最上面的 io.TextIOWrapper 层
+str = 'ceshi xiao ice'
+u = io.StringIO(str)
+f = io.TextIOWrapper(u, encoding='utf-8')
+print(f.buffer)
+    # Python 将字节写入文本文件
+    # 能够通过读取文本文件的 buffer 属性来读取二进制数据
+    # I/O系统以层级结构的形式构建而成。 文本文件是通过在一个拥有缓冲的二进制模式文件上增加一个Unicode编码/解码层来创建。 buffer 属性指向对应的底层文件。如果你直接访问它的话就会绕过文本编码/解码层
+# sys.stdout.write(b'Hello\n')
+sys.stdout.buffer.write(b'Hello\n')
 
 
 
